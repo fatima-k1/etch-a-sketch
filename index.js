c52e504a6b236fe569d8301a06c10b90844eb000
fatima-k1 <khandwalla.f@gmail.com>
2022-11-16 08:47:11 +0300

    added an event listener for squares to change color

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 //create a 16x16 grid of square divs
-const container = document.querySelector('.container');
-container.textContent = '';
+function createGrid(num) {
+    const container = document.querySelector('.container');
+    container.textContent = '';
 
-for (let i = 0; i < 16*16; i++) {
-let cell = document.createElement('div');
-cell.className = 'grid-cell';
-container.appendChild(cell);
+    let gridSize = num*num
+    let width = container.clientWidth/num;
+    let height = container.clientHeight/num;
+    for (let i = 0; i < gridSize; i++) {
+        let cell = document.createElement('div');
+        cell.style.width = `${width}px`;
+        cell.style.height = `${height}px`;
+        cell.className = 'grid-cell';
+        container.appendChild(cell);
+    }
 }
+
+createGrid(16);
+
+//Set up a “hover” effect so that the cells change color when your mouse passes over them
+let cells = document.querySelectorAll('.grid-cell');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () =>
+        cell.style.backgroundColor = 'black');
+    })
